48da493a0e71f815c02d6fb1afec68c9b1582890
fragburger@gmail.com <fragburger@gmail.com@81b6a9b0-243e-da85-4ec9-fea869dd8026>
2011-10-30 01:03:35 +0000

    Styling

---
+++ SS-Mobile-Two/MiniGame.TakePhotoAndPlay/sdkSilverlightXNACS/sdkSilverlightXNACS/ApplicationHome.xaml.cs
@@ -0,0 +1,22 @@
+using Microsoft.Phone.Controls;
+
+namespace sdkPhotosCS
+{
+    public partial class ApplicationHome : PhoneApplicationPage
+    {      
+        public ApplicationHome()
+        {
+            InitializeComponent();
+        }
+
+        private void NavigateToSinglePlayerMode()
+        {
+            
+        }
+
+        private void NavigateToMultiPlayerMode()
+        {
+
+        }
+    }
+}
